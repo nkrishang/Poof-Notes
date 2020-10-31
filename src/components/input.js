a0dbeded73310c618fa9d061bba7c3da335acb27
nkrishang <62195808+nkrishang@users.noreply.github.com>
2020-10-31 14:06:27 -0400

    STT update

--- src/components/input.js
+++ src/components/input.js
@@ -5,24 +5,122 @@ import '../output.css'
 import { Collapse } from '@material-ui/core';
 
 import attachLogo from '../assets/paperclip.svg';
+import micLogo from '../assets/mic.svg';
 
-function NoteInput({handleNoteText, handleNoteTitle, handleStyle, handleSubmit, handleFiles, color, noteState}) {
+import io from 'socket.io-client';
+import RecordRTC, { StereoAudioRecorder } from 'recordrtc';
+import ss from 'socket.io-stream';
+
+function NoteInput({handleNoteText, handleNoteTitle, handleStyle, handleSubmit, handleFiles, handleTranscription, color, noteState}) {
 
     const {noteText, title, file} = noteState;
     const collapseState = (title || noteText) ? true : false; 
+
+    const [recording, setRecording] = React.useState(false)
+    const [audioObjects, setAudioObjects] = React.useState({
+        socket: null,
+        recordRtc: null,
+        mediaStream: null
+    })
+
+    React.useEffect(() => {
+        const socketio = io("https://poofnotes.herokuapp.com");
+        // const socketio = io("http://localhost:8080");
+        const socket = socketio.on('connect', () => {
+          setRecording(false)
+          console.log('React socket client connected.')
+        });
+        setAudioObjects(audioObjects => {
+          const newObj = {...audioObjects, socket: socket};
+          return newObj;
+        });
+    
+        socket.on('results', (data) => {
+          if(data && data.results[0] && data.results[0].alternatives[0]) {
+            const newText = data.results[0].alternatives[0].transcript;
+            handleTranscription(newText);
+          }
+        });
+    
+        return () => socketio.disconnect();
+// eslint-disable-next-line
+    }, [])
+
+    function startRecording() {
+
+        setRecording(true)
+    
+        navigator.getUserMedia({
+          audio: true
+        }, (stream) => {
+    
+          const recordAudio = RecordRTC(stream, {
+            type: 'audio',
+            mimeType: 'audio/webm',
+            sampleRate: 44100, 
+    
+            recorderType: StereoAudioRecorder,
+    
+            numberOfAudioChannels: 1,
+    
+            timeSlice: 3000,
+    
+            desiredSampRate: 16000,
+    
+            ondataavailable: function(blob) {
+    
+              const stream = ss.createStream();
+    
+              ss(audioObjects.socket).emit('stream-transcribe', stream, {
+                  name: 'stream.wav', 
+                  size: blob.size
+              });
+              ss.createBlobReadStream(blob).pipe(stream);
+            }
+          });
+    
+          recordAudio.startRecording();
+          setAudioObjects({...audioObjects, recordRtc: recordAudio, mediaStream: stream})
+    
+        }, function(error) {
+            console.error(JSON.stringify(error));
+        });
+    
+    }
+    
+    function stopRecording() {
+    
+        audioObjects.recordRtc.stopRecording();
+        
+        audioObjects.mediaStream.getTracks().forEach((track) => {
+          track.stop();
+        });
+    
+        setAudioObjects({...audioObjects, recordRtc: null, mediaStream: null});
+        setRecording(false)
+    }
+      
 
     return (
         <div className="Note m-4">
             
             <div className="h-auto rounded-md shadow-lg bg-white" style={{width: "500px"}}>
 
-                <div>
-                    <input value={title} className="mx-6 my-2 p-4 leading-5 font-bold text-gray-900" maxLength="30" style={{width: "450px"}} placeholder={noteText ? "Enter title" : "Take a note..."} onChange={handleNoteTitle}/>
+                {/* <div>
+                    <input value={title} className="mx-6 my-2 p-4 leading-5 font-bold text-gray-900 border border-solid-2 border-black" maxLength="30" style={{width: "450px", height: "50px"}} placeholder={noteText ? "Enter title" : "Take a note..."} onChange={handleNoteTitle}/>
+                </div> */}
+
+                <div className="flex space-around">
+                    <input value={title} className="mx-6 my-2 p-4 leading-5 font-bold text-gray-900" maxLength="30" style={{width: "450px", height: "50px"}} placeholder={noteText ? "Enter title" : "Take a note..."} onChange={handleNoteTitle}/>
+
+                    <button className="px-4 m-2 rounded" onClick={!recording ? startRecording : stopRecording} style={{backgroundColor: `${recording ? "#FC8181" : ""}`, height: "50px"}}>
+                        <img src={micLogo} alt="mic" className="opacity-25"/>
+                    </button>
                 </div>
 
                 <Collapse in={collapseState} timeout='auto'>
                     <div>
-                        <textarea value={noteText} onChange={handleNoteText} style={{resize: "none", width: "450px"}} rows="7" className="mx-6 my-2 p-4 font-mono" maxLength="255" placeholder={collapseState ? 'Take a note...' : ''}>
+                        <textarea value={noteText} onChange={recording ? () => {} : handleNoteText} style={{resize: "none", width: "450px"}} wrap="hard" rows="7" className="mx-6 my-2 p-4 font-mono" maxLength="255" placeholder={collapseState ? 'Take a note...' : ''}>
                         </textarea>
 
                         <div className="flex justify-end">
