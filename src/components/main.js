7937d4ca3b74b415c9b1359a01e4bc1074766684
nkrishang <62195808+nkrishang@users.noreply.github.com>
2020-10-31 18:36:37 -0400

    Cleaned state and useLocalStorageState

--- src/components/main.js
+++ src/components/main.js
@@ -3,6 +3,8 @@ import React from 'react';
 import '../App.css';
 import '../output.css'
 
+import {useLocalStorageState} from '../utils/hooks'
+
 import NoteInput from './input';
 import StickyNote from './sticky';
 
@@ -13,14 +15,13 @@ function Main({user, loading}) {
   //Clean up state
   const [notes, setNotes] = React.useState([])
 
-  const [noteState, setNoteState] = React.useState({
+  const [noteState, setNoteState] = useLocalStorageState('noteContent', {
     title: '',
     noteText: '',
     noteStyle: "rounded-lg shadow-lg bg-indigo-200",
-    file: null
+    color: "indigo"
   })
-
-  const [color, setColor] = React.useState("indigo");
+  const [noteFile, setNoteFile] = React.useState(null);
 
   React.useEffect(() => {
 
@@ -50,7 +51,8 @@ function Main({user, loading}) {
     
     setNoteState((noteState) => {
       const {noteText} = noteState;
-      const newState = {...noteState, noteText: (noteText + newText + ' ')};
+      const preAudioText = noteText ? noteText + ' ' : '';
+      const newState = {...noteState, noteText: (preAudioText + newText)};
       setNoteState(newState);
     })
   }
@@ -60,21 +62,20 @@ function Main({user, loading}) {
   }
 
   function handleStyle(event) {
-
-    setColor(event.target.getAttribute('name'));
-
-    switch(event.target.getAttribute('name')) {
+    const selectedColor = event.target.getAttribute('name');
+    
+    switch(selectedColor) {
       case "indigo" :
-        setNoteState({...noteState, noteStyle: "rounded-lg shadow-lg bg-indigo-200"})
+        setNoteState({...noteState, color: selectedColor, noteStyle: "rounded-lg shadow-lg bg-indigo-200"})
         break;
       case "green" :
-        setNoteState({...noteState, noteStyle: "rounded-lg shadow-lg bg-green-200"})
+        setNoteState({...noteState, color: selectedColor, noteStyle: "rounded-lg shadow-lg bg-green-200"})
         break;
       case "yellow" :
-        setNoteState({...noteState, noteStyle: "rounded-lg shadow-lg bg-yellow-200"})
+        setNoteState({...noteState, color: selectedColor, noteStyle: "rounded-lg shadow-lg bg-yellow-200"})
         break;
       case "pink" :
-        setNoteState({...noteState, noteStyle: "rounded-lg shadow-lg bg-pink-200"})
+        setNoteState({...noteState, color: selectedColor, noteStyle: "rounded-lg shadow-lg bg-pink-200"})
         break;
       default:
       break;
@@ -83,7 +84,8 @@ function Main({user, loading}) {
 
   function handleFiles(event) {        
     const file = event.target.files[0];
-    setNoteState({...noteState, file:file});
+    // setNoteState({...noteState, file:file});
+    setNoteFile(file)
 
     const {name, size, type} = file;
     console.log(name, size, type);
@@ -93,27 +95,27 @@ function Main({user, loading}) {
     let newItem;
 
     if(!noteState.title) {
-      newItem = {...noteState, title: "New Note"};
+      newItem = {...noteState, file: noteFile, title: "New Note"};
     } else {
-      newItem = {...noteState}
+      newItem = {...noteState, file: noteFile}
     }
     const {title, noteText} = noteState
     const itemId = sha256((title.concat(noteText)).concat(Math.random().toString().substring(2))).toString();
 
     userbase.insertItem({databaseName: "notes", item: newItem, itemId: itemId})
     .then(() => {
-      if(noteState.file) {
-        userbase.uploadFile({databaseName: "notes", itemId: itemId, file: noteState.file})
+      if(noteFile) {
+        userbase.uploadFile({databaseName: "notes", itemId: itemId, file: noteFile})
         .then(() => {
-          setNoteState({...noteState, title: "", noteText: "", file: null})
-          // console.log("file successfully uploaded");
+          setNoteFile(null)
+          console.log("file successfully uploaded");
         })
         .catch((e) => {
           alert("Sorry, there was an error when uploading your file. Please try again.")
           console.log(e);
         })
       }
-      setNoteState({...noteState, title: "", noteText: "", file: null})
+      setNoteState({...noteState, title: "", noteText: ""})
     })
     .catch((e) => {
       alert("Sorry, there was an error when making your note. Please try again.")
@@ -127,7 +129,7 @@ function Main({user, loading}) {
       
       <div className="flex justify-center">
           <NoteInput handleNoteText={handleNoteText} handleNoteTitle={handleNoteTitle} handleTranscription={handleTranscription}
-          handleStyle={handleStyle} handleSubmit={handleSubmit} handleFiles={handleFiles} color={color} noteState={noteState}/>
+          handleStyle={handleStyle} handleSubmit={handleSubmit} handleFiles={handleFiles} file={noteFile} noteState={noteState}/>
       </div>
 
       <div className="m-auto my-4 w-4/5 h-auto">
